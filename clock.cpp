c15ab8006b57c7787288bdb24f2fb20c30d86489
Kelyane <kelyane.lokita@gmail.com>
2016-11-03 21:31:22 +0100

    Added others pointers

--- clock.cpp
+++ clock.cpp
@@ -10,12 +10,19 @@ Clock::Clock(QWidget *parent) :
     ui->graphicsView->setScene(scene);
 
     QBrush redBrush(Qt::red);
-    QBrush blueBrush(Qt::blue);
     QPen blackPen(Qt::black);
-    blackPen.setWidth(6);
+    QPen bluePen(Qt::blue);
+    QPen greenPen(Qt::green);
+
+    blackPen.setWidth(3);
+    bluePen.setWidth(3);
+    greenPen.setWidth(3);
 
     ellipse = scene->addEllipse(0,0,100,100,blackPen,redBrush);
     hourPointer = scene->addLine(0,0,80,80,blackPen);
+    minutePointer = scene->addLine(10,10,50,50,bluePen);
+    secondPointer = scene->addLine(0,0,20,20,greenPen);
+
 }
 
 Clock::~Clock()
